5beb883f096e99bff7c720bb129d47f6269db61e
berkay <mberkay@yahoo.com>
2014-03-29 15:31:35 -0400

    Update opsgenie_test.rb

--- test/opsgenie_test.rb
+++ test/opsgenie_test.rb
@@ -1,12 +1,12 @@
 require File.expand_path('../helper', __FILE__)
 
-class PagerdutyTest < PapertrailServices::TestCase
+class OpsGenieTest < PapertrailServices::TestCase
   def setup
     @stubs = Faraday::Adapter::Test::Stubs.new
   end
 
   def test_logs
-    svc = service(:logs, { :service_key => 'k' }, payload)
+    svc = service(:logs, { :api_key => 'k' }, payload)
 
     @stubs.post '/generic/2010-04-15/create_event.json' do |env|
       [200, {}, '']
@@ -14,19 +14,8 @@ class PagerdutyTest < PapertrailServices::TestCase
 
     svc.receive_logs
   end
-
-  def test_logs_with_incident_key
-    svc = service(:logs, { :service_key => 'k', :incident_key => '%HOST%/PAPERTRAIL' }, payload)
-
-    @stubs.post '/generic/2010-04-15/create_event.json' do |env|
-      [200, {}, '']
-    end
-
-    svc.receive_logs
-  end
-
 
   def service(*args)
-    super Service::Pagerduty, *args
+    super Service::OpsGenie, *args
   end
 end
